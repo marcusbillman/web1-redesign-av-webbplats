067a3880d3c6ba031a00d05cd9ae4c44d5d7cb0d
Marcus Billman <xfoxx37@gmail.com>
2020-02-13 15:36:17 +0100

    Make dark theme toggle functional

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,8 @@
-console.log("loaded");
+const darkThemeToggle = document.getElementById("darkThemeToggle");
+
+let darkThemeEnabled = false;
+
+darkThemeToggle.addEventListener("click", () => {
+    darkThemeEnabled = !darkThemeEnabled;
+    document.body.setAttribute("data-dark-theme", darkThemeEnabled);
+});
